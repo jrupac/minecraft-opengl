d7094b76ee3d82aa44cd21059143b71be36efce4
sytang <none@none>
2011-05-08 16:54:45 -0400

    fixed small bug

--- src/minecraft.cpp
+++ src/minecraft.cpp
@@ -20,7 +20,7 @@ static char *output_image_name = NULL;
 
 // Display variables
 
-static bool startMenu = false;
+static bool startMenu = true;
 
 static R3Scene *scene = NULL;
 static int show_faces = 1;
